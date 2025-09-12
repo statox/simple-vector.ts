7a612894747c838bcf1be65de41f7df2d18d24c1
statox <me@statox.fr>
2025-09-13 00:19:46 +0200

    utils - create file with getClampedValue function

---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+/**
+ * An exception thrown by some methods when an expected number is invalid.
+ */
+export class InvalidNumberError extends Error {
+    constructor(val: unknown) {
+        super('Expected a number, instead got', val);
+    }
+}
+
+const validateNumber = (val: unknown): number => {
+    if (typeof val !== 'number' || !Number.isFinite(val)) {
+        throw new InvalidNumberError(val);
+    }
+    return val;
+};
+
+export const getClampedValue = (current: number, minBound: number, maxBound: number) => {
+    validateNumber(current);
+    validateNumber(minBound);
+    validateNumber(maxBound);
+    return Math.min(Math.max(current, minBound), maxBound);
+};
